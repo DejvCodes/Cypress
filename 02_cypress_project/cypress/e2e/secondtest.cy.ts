7a97559f9dc0bc96cc3c09d8c642c5413ca63586
David Kalmus <dejvios@seznam.cz>
2025-11-04 15:07:07 +0100

    cypress

--- 02_cypress_project/cypress/e2e/secondtest.cy.ts
+++ 02_cypress_project/cypress/e2e/secondtest.cy.ts
@@ -9,14 +9,14 @@ Testovací soubor secondtest.cy.ts (test suite file)
 describe('první sada testů', () => { // describe() definuje sadu testů s názvem 'první sada testů'
 
   it('test #1', () => {
-    cy.log('Zde je test #1') // cy.log() vypíše zprávu do konzole Cypress
-  }), // it() definuje jednotlivý test s názvem 'test #1'
+    cy.log('Zde je test #1'); // cy.log() vypíše zprávu do konzole Cypress
+  }) // it() definuje jednotlivý test s názvem 'test #1'
 
   it('test #2', () => {
-    cy.log('Zde je test #2') // cy.log() vypíše zprávu do konzole Cypress    
+    cy.log('Zde je test #2'); // cy.log() vypíše zprávu do konzole Cypress    
   }) // it() definuje jednotlivý test s názvem 'test #2'
 
   it('test #3', () => {
-    cy.log('Zde je test #3') // cy.log() vypíše zprávu do konzole Cypress
+    cy.log('Zde je test #3'); // cy.log() vypíše zprávu do konzole Cypress
   }) // it() definuje jednotlivý test s názvem 'test #3'
 }) 
